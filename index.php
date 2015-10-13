5885ee8d26c53f24615ad119092561b9e8397071
RubenCox <cox.ruben@hotmail.com>
2015-10-13 15:02:19 +0200

    Wire-Frame Uitwerking Versie 1.0
    Een beetje spelen met het concept

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 </head>
 <body ng-app>
 <?php
+	include "html/nav.html";
 	include "php/home.php";
   ?>
 
--- php/home.php
+++ php/home.php
@@ -1,4 +1,77 @@
-<h1>Home pagina</h1>
-<h2>{{2+2}}</h2>
+<div class="container-fluid ">
+	<div class="row">
+		<div class="col-md-8 col-md-offset-2">
 
-<p>My first paragraph.</p>
+		<!-- Stack the columns on mobile by making one full-width and the other half-width -->
+		<div class="row">
+		  <div class="col-md-8 border">
+			<div class="row">
+				<div class="col-md-12  border">
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed posuere ex non tortor dapibus, ac condimentum diam tempus. Nunc id bibendum mi. Integer vel semper erat, id porta odio. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. In vitae arcu ut elit aliquam feugiat. Vivamus sollicitudin velit eget convallis molestie. In quis nunc suscipit, consectetur magna sed, venenatis diam.
+
+Donec non leo id lacus gravida dapibus et eu metus. Pellentesque finibus porttitor quam ac tempor. Vestibulum tincidunt ullamcorper elit quis dapibus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed sodales, diam nec blandit ullamcorper, justo urna elementum mauris, faucibus rutrum augue leo venenatis est. Phasellus pellentesque nibh justo, consequat venenatis arcu tristique nec. Pellentesque accumsan bibendum tempus. Donec purus augue, porta sit amet placerat quis, mattis eu tortor. Donec lacinia fringilla dolor eu suscipit. Maecenas at eleifend massa. Integer aliquet justo nec ante eleifend, vitae luctus sem faucibus. Nullam enim dolor, suscipit vel bibendum sed, vulputate quis lacus. Phasellus vel commodo tellus. Duis molestie convallis tellus sed elementum. Proin porttitor lorem vitae nibh dignissim lobortis.
+					</p>
+				</div>
+			</div>
+		
+			<div class="row">
+				<div class="col-md-12  border">
+					<ul class="ch-grid">
+						<li>
+							<div class="ch-item ch-img-1 img-responsive">
+								<div class="ch-info">
+									<div class="col-md-6 col-md-offset-3">
+									<form class="form-signin">
+										<h2 class="form-signin-heading">Login</h2>
+										<label for="inputEmail" class="sr-only">Email adres</label>
+										<input type="email" id="inputEmail" class="form-control" placeholder="Email adres" required autofocus>
+										<label for="inputPassword" class="sr-only">Wachtwoord</label>
+										<input type="password" id="inputPassword" class="form-control" placeholder="Wachtwoord" required>
+										<div class="checkbox">
+										  <label>
+											<input type="checkbox" value="remember-me"> Mij onthouden
+										  </label>
+										</div>
+										<button class="btn btn-lg btn-primary btn-block" type="submit">Login</button>
+									</form>
+				</div>
+										<p><a href="http://drbl.in/eOPF">Wachtwoord vergeten</a><a href="http://drbl.in/eOPF">Registreer</a></p>
+								</div>
+							</div>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div class="row">
+			</div>
+		  </div>
+		  <div class="col-md-4 border">
+			<div class="row">
+				<div class="col-xs-4 col-sm-4 col-md-12 border">
+					<h1 class="prijsTitel">Hoofdprijs</h1>
+					<img src="assets/img/fruit/fordShelby.jpg" class="img-responsive banner prijs">
+				</div>
+			
+			
+				<div class="col-xs-4 col-sm-4 col-md-12 border">
+					<h1 class="prijsTitel">Premiumprijs</h1>
+					<img src="assets/img/fruit/vespaGeel.jpg" class="img-responsive banner prijs">
+				</div>
+		
+			
+				<div class="col-xs-4 col-sm-4 col-md-12 border">
+					<h1 class="prijsTitel">Troostprijs</h1>
+					<img src="assets/img/fruit/hotwheels.jpg" class="img-responsive banner prijs">
+				</div>
+			</div>	
+		  </div>
+		</div>
+
+		</div>		
+	</div>
+</div>
+
+	
+
+
