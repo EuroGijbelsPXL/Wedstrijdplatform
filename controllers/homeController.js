0bc494920b7e529862d682574377df30861acfea
RubenCox <cox.ruben@hotmail.com>
2015-11-18 15:32:47 +0100

    Redmine Push Test

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -7,7 +7,7 @@
 		function homeController() {
 					this.msg = 'Hello vanuit de homeController';
 					
-					this.uitleg = 'Speluitleg van de wedstrijdpagina,Speluitleg van de wedstrijdpagina,Speluitleg van de wedstrijdpagina';
+					this.uitleg = 'ClemenGold is een heerlijk zoete, geurige en zachte mandarijn. Zongerijpt en met de hand geplukt door geselecteerde telers over de hele wereld, belooft een ClemenGold een versheid waar je iedere dag van kan genieten. Ben jij een fresh food-lover? Op zoek naar lekkere en gezonde, verse producten? Wil je de nieuwste producten als eerste ontdekken en proeven? Of wil je op de hoogte gebracht worden van acties en promoties? Schrijf je dan in voor onze nieuwsbrief! Ontdek meer over ClemenGold, of probeer deze recepten uit!';
 					this.prijzen = 	[{'type': 'Hoofdprijs', 'afb': 'fordShelby.jpg'},
 							{'type': 'Premiumprijs', 'afb': 'vespaGeel.jpg'},
 							{'type': 'Troostprijs', 'afb': 'hotwheels.jpg'}];
